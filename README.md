fa3119e406a1dc6e3b70a8b4774332375bb5df6f
Max le Fou <buttbadger@gmail.com>
2020-02-24 22:03:33 +0100

    first release

---
+++ README.md
@@ -0,0 +1,6 @@
+# Convertisseur Euro > McDo
+Ceci convertit en Big Mac et frites une somme en Euros, comme le font les Rois Mages dans le film des Inconnus. Honte à vous si vous n'avez pas vu ce film. :p
+
+[Vous pouvez tester le convertisseur ici.](https://maxlefou.gitlab.io/mcdo-convert)
+
+![Toute ma sympathie aux Inconnus!](pic.jpg)
---
+++ script.js
@@ -0,0 +1,24 @@
+function mcdoConvert() {
+    // Prix basés sur ceux d'un big mac et d'une frite moyenne en février 2020
+    var inputEuro = document.getElementById('inputEuro'),
+        bigmacDisp = document.getElementById('bigmac'),
+        friteDisp = document.getElementById('frite'),
+        errorMsg = document.getElementById('errorMsg'),
+        bigmac = 4.55,
+        frite = 2.65,
+        err;
+
+    // Le garde-fou
+    if (inputEuro.value == "" || isNaN(inputEuro.value) || inputEuro.value < 1) {
+        errorMsg.classList.remove("d-none");
+        return false;
+    } else {
+        errorMsg.classList.add("d-none");
+    };
+
+    // L'opération. On divise pour les bigmacs et on prend le modulo pour les frites, puis on arrondit avec toFixed.
+    operation1 = inputEuro.value / bigmac;
+    operation2 = inputEuro.value % frite;
+    bigmacDisp.innerHTML = operation1.toFixed(0);
+    friteDisp.innerHTML = operation2.toFixed(0);
+};
